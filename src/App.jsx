a3c33f4925f4c6638e8463c6db6eccfcde0a7d0b
NarendarPaindla <narendarreddypaindla@gmail.com>
2025-02-07 05:50:14 +0000

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,23 @@
 import Header from './components/Header';
 import './App.css'
+import Greetings from './components/Greetings';
+import Counter from './components/Counter';
 
 function App(){
   const name="iotalpha";
   const course="react";
   const items=['item1','item2', 'item3','item4'];
+  
 return(
 <>
+<Greetings/>
 <Header name={name} />
 <h1>Welcome</h1>
 <p>Here {name} is learning {course}</p>
 <ul>
   {items.map(item=><li key={item}>{item}</li>)}
 </ul>
+<Counter/>
 </>
 );
 }
---
+++ src/components/Greetings.jsx
@@ -0,0 +1,24 @@
+// import React, { Component } from 'react';
+
+// class Greeting extends Component {
+//   render() {
+//     return <h1>Hello, CSBETA</h1>;
+//   }
+// }
+
+// export default Greeting;
+
+// function Greetings(){
+//     return(
+// <h1>Hello i am functional component</h1>
+//     );
+    
+// }
+// export default Greetings;
+
+const Greetings=()=>{
+    return(
+        <h1>Hello i am functional component</h1>
+            );
+}
+export default Greetings;
