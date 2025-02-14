4a91733924d616949b507a99f240adcace8b3c96
narendarpaindla <narendarreddypaindla@gmail.com>
2025-02-14 05:18:44 +0000

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,12 @@ import React from 'react'
 import UseEffectDependencies from './components/UseEffectDependencies';
 import Parent from './components/Parent';
 import Child from './components/Child';
+import MyForm from './components/MyForm';
 function App(){
   return(
     <>
-  
-  <Parent/>
+  <MyForm/>
+ 
     </>
   )
 }
